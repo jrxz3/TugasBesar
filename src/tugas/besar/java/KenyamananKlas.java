099c76ab326ab701c54dcdc3954da8ec062497d3
Priestanto Priyambodo <jrx.appolo@gmail.com>
2015-05-12 11:35:50 +0700

    Update KenyamananKlas.java

--- src/tugas/besar/java/KenyamananKlas.java
+++ src/tugas/besar/java/KenyamananKlas.java
@@ -1,15 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package tugas.besar.java;
 
-/**
- *
- * @author Jrx Acer
- */
+import java.util.Scanner;
+
 public class KenyamananKlas {
-    
+    public void KenyamananKlas() {
+        System.out.println("Kondisi Kebisingan : ");
+        System.out.println("\n1. tidak bising \n2. bising");
+	super.setKebisingan(in.nextInt());
+	System.out.println("\n");
+	System.out.println("kondisi bau ruangan : ");
+        System.out.println("\n1. tidak bau \n2. bau");
+	super.setBau(in.nextInt());
+	System.out.println("\n");
+	System.out.println("Kondisi Kebocoran ruangan : ");
+        System.out.println("\n1. tidak bocor \n2. bocor");
+	super.setKebocoran(in.nextInt());
+	System.out.println("\n");
+	System.out.println("Konsisi kerusakan ruangan : ");
+        System.out.println("\n1. tidak rusak \n2. rusak");
+	super.setKerusakan(in.nextInt());
+	System.out.println("\n");
+	System.out.println("Kondisi Keausan Ruangan : ");
+        System.out.println("\n1.tidak aus \n2. sudah aus");
+	super.setKeausan(in.nextInt());
+	System.out.println("\n");
+	}
+
+	public String AnalisisKebisingan(){
+		if(super.getKebisingan()==1)
+			return("Tingkat Kebisingan = Sesuai");
+		else
+			return("Tingkat Kebisingan = tidak sesuai");
+	}
+	public String AnalisisBau(){
+		if(super.getBau()==1)
+			return("Bau Ruangan = Sesuai");
+		else
+			return("Bau Ruangan = tidak sesuai");
+	}
+	public String AnalisisKebocoran(){
+		if(super.getKebocoran()==1)
+			return("Tingkat Kebocoran = Sesuai");
+		else
+			return("Tingkat Kebocoran = tidak sesuai");
+	}
+	public String AnalisisKerusakan(){
+		if(super.getKerusakan()==1)
+			return("Tingkat Kerusakan = Sesuai");
+		else
+			return("Tingkat Kerusakan = tidak sesuai");
+	}
+	public String AnalisisKeausan(){
+		if(super.getKeausan()==1)
+			return("Tingkat Keausan = Sesuai");
+		else
+			return("Tingkat Keausan = tidak sesuai");
+	}
 }
