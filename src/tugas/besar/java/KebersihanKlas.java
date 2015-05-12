0b4f2d24c9a483a681e209febb05bb9cdbcc3e0e
Priestanto Priyambodo <jrx.appolo@gmail.com>
2015-05-12 11:34:07 +0700

    Update KebersihanKlas.java
    Update KebersihanKlas.java

--- src/tugas/besar/java/KebersihanKlas.java
+++ src/tugas/besar/java/KebersihanKlas.java
@@ -1,15 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package tugas.besar.java;
 
-/**
- *
- * @author Jrx Acer
- */
+import java.util.Scanner;
+
 public class KebersihanKlas {
-    
+    Scanner in = new Scanner(System.in);
+    public void KebersihanKls(){
+    System.out.println("Sirkulasi udara = ");
+        super.setSirkulasiUdara(in.nextInt());
+        System.out.println("\n");
+        System.out.println("Nilai Pencahayaan = ");
+        super.setNilaiPencahayaan(in.nextInt());
+        System.out.println("\n");
+        System.out.println("Kelembapan = ");
+        super.setKelembapan(in.nextInt());
+        System.out.println("\n");
+        System.out.println("Suhu = ");
+        super.setSuhu(in.nextInt());
+        System.out.println("\n");
+    }
+    public String AnalisisSirkulasiUdara(){
+		if(super.getSirkulasiUdara()==1)
+			return("Sirkulasi Udara = sesuai");
+		else
+			return("Sirkulasi Udara = tidak sesuai");
+		
+	}
+	public String AnalisisPencahayaan(){
+		if(super.getNilaiPencahayaan()>=250&&super.getNilaiPencahayaan()<=350)
+			return("Pencahayaan = sesuai");
+		else
+			return("Pencahayaan = tidak sesuai");
+	}
+	public String AnalisisKelembapan(){
+		if(super.getKelembapan()>=70&&super.getKelembapan()<=80)
+			return("Kelembapan = sesuai");
+		else
+			return("Kelembapan = tidak sesuai");
+	}
+	public String AnalisisSuhu(){
+		if(super.getSuhu()>=25&&super.getSuhu()<=35)
+			return("Suhu = sesuai");
+		else
+			return("Suhu = tidak sesuai");
+	}
 }
